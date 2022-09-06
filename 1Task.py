6e836563785dae08da265bd49526114c6e2cf538
Nadya <spinakernadya7@gmail.com>
2022-09-07 02:31:05 +0300

    1 and 2 task

--- 1Task.py
+++ 1Task.py
@@ -1,13 +1,12 @@
 
-def my_code(data, n=1):
+def my_code(data, n=0):
     for key, value in data.items():
-        print(key, ':', sep='')
+        print(n*'\t',key, ':', sep='')
         if isinstance(value, dict):
             data1={**value}
-            print('\t', end='')
             my_code(data1, n+1)
         else:
-            print(n*'\t', value, sep='')
+            print((n+1)*'\t', value, sep='')
 
 my_code({
  'first': 'first_value',
@@ -20,12 +19,14 @@ my_code({
  },
  '2': '2/value'
 })
-
+#добавила обещанный пример с ещё одним вложенным словарём
 my_code({
  '1': {
  'child': '1/child/value',
+     'child1':
+         {
+             '1/child1':'1/child1/value'
+         }
  },
  '2': '2/value'
 })
-
-#my_code({1: {'Name': 'Ashish', 'Age': 33, 'Designation': 'Web Developer'}, 2: {'Name': 'Shubham', 'Age': 23, 'Designation': 'IOS APP Developer'}, 3: {'Name': 'Vandana', 'Age': 25, 'Designation': 'Data Scientist'}})
--- 2Task.py
+++ 2Task.py
@@ -1,27 +1,25 @@
-def my_code(obj, key, list1=[], n=1):
-    while n==1:
-        list1.append(key)# добавляю в лист вершину с которой работаю
-        if key in obj.keys():
-            if isinstance(obj, dict):
-                for k, v in obj.items():
-                    if isinstance(v, (list)):
-                        for v1 in v:
-                            if (v1 in obj[key] and k==key): #условия смежной вершины: 1) значение есть в словаре под номером родительской
+def my_code(obj, key, list1=None):
+    if list1 is None:
+        list1 = []
+    list1.append(key)  # добавляю в лист вершину с которой работаю
+    if key in obj.keys():
+        if isinstance(obj, dict):
+            for k, v in obj.items():
+                if isinstance(v, (list)):
+                    for v1 in v:
+                        if (v1 in obj[key] and k==key): #условия смежной вершины: 1) значение есть в словаре под номером родительской
                                                             # 2) номер совпадает с номером родительской
-                                if v1 not in obj.keys() or len(obj[v1])==1: #убеждение, что это вершина крайняя или что связана лишь с одной
-                                    obj[key].remove(v1)
-                                my_code(obj, v1, list1,1)
-        else: # пошел заново с первой вершины другие пути искать
-            my_code(obj, list1[0], list1,1)
-        n=0
-
+                            if v1 not in obj.keys() or len(obj[v1])==1: #убеждение, что это вершина крайняя или что связана лишь с одной дочерней вершиной
+                                obj[key].remove(v1)
+                            return my_code(obj, v1, list1)
+    else: # пошел заново с первой вершины другие пути искать
+        return my_code(obj, list1[0], list1)
     return print_answer(list1)
 
-def print_answer(l):
+def print_answer(l): #привожу к формату вывода, так как в лист складываются все маршруты к конечным вершинам
     answer = []
     [answer.append(x) for x in l if x not in answer]
     print(*answer, sep='\n')
-
 
 data = {
  1: [2, 3],
@@ -32,10 +30,8 @@ data1 = {
  2: [3, 4],
  4: [1]
 }
-#my_code(data, 1)
-
 my_code(data, 1)
-#my_code(data1, 1)
+my_code(data1, 1)
 
 
 
